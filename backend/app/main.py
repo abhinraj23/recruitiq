9f01e89ea6657bcf046e1378fe2807673ba9e140
ABHINRAJ N A <144911680+abhinraj23@users.noreply.github.com>
2026-08-21 15:03:16 +0000

    add main py for embedding

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from contextlib import asynccontextmanager
 from app.api.routes import router
 from app.api.auth import router as auth_router
 from app.api.resumes import router as resumes_router
-from app.api.jobs import router as job_router
+from app.api.jobs import router as job_router 
 from app.core.config import settings
 from app.db.database import create_db_and_tables
 
@@ -23,4 +23,3 @@ app.include_router(router)
 app.include_router(auth_router)
 app.include_router(resumes_router)
 app.include_router(job_router)
-
